6daf52549a0d6e0b3633944eed2081f236062438
Lefhika <lefhikamolebatsi@gmail.com>
2025-02-10 14:52:00 +0200

    RSI & Stk Osc

--- Robots/PendingOrders Sample/PendingOrders Sample/Properties/AssemblyInfo.cs
+++ Robots/PendingOrders Sample/PendingOrders Sample/Properties/AssemblyInfo.cs
@@ -1,20 +1,72 @@
-﻿using System.Diagnostics;
-using System.Reflection;
-using System.Runtime.InteropServices;
+using cAlgo.API;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
 
-[assembly: AssemblyTitle("PendingOrders Sample")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("PendingOrders Sample")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+namespace cAlgo.Robots
+{
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class NewsEventPendingOrders : Robot
+    {
+        // Parameters for the news event
+        [Parameter("News Event Time (UTC)", DefaultValue = "2023-10-30 12:00:00")]
+        public string NewsEventTime { get; set; }
 
-[assembly: ComVisible(false)]
+        [Parameter("Buy Stop Distance (Pips)", DefaultValue = 20)]
+        public int BuyStopDistance { get; set; }
 
-[assembly: Guid("35f19c43-c7cc-4f53-b8e0-61caaf9cd8e8")]
+        [Parameter("Sell Stop Distance (Pips)", DefaultValue = 20)]
+        public int SellStopDistance { get; set; }
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-#if DEBUG
-    [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.DisableOptimizations)]
-#endif
+        [Parameter("Lot Size", DefaultValue = 0.1)]
+        public double LotSize { get; set; }
+
+        [Parameter("Take Profit (Pips)", DefaultValue = 40)]
+        public int TakeProfitPips { get; set; }
+
+        [Parameter("Stop Loss (Pips)", DefaultValue = 20)]
+        public int StopLossPips { get; set; }
+
+        private DateTime _newsEventDateTime;
+
+        protected override void OnStart()
+        {
+            // Convert the news event time string to a DateTime object
+            _newsEventDateTime = DateTime.Parse(NewsEventTime);
+
+            // Check if the news event is in the future
+            if (_newsEventDateTime <= Server.Time)
+            {
+                Print("News event time must be in the future. Please adjust the parameter.");
+                Stop();
+                return;
+            }
+
+            Print("cBot started. Waiting for news event time...");
+        }
+
+        protected override void OnBar()
+        {
+            // Check if it's time to place pending orders
+            if (Server.Time >= _newsEventDateTime.AddMinutes(-15) && Server.Time < _newsEventDateTime)
+            {
+                PlacePendingOrders();
+                Stop(); // Stop the cBot after placing orders
+            }
+        }
+
+        private void PlacePendingOrders()
+        {
+            // Calculate Buy Stop and Sell Stop levels
+            double buyStopPrice = Symbol.Bid + BuyStopDistance * Symbol.PipSize;
+            double sellStopPrice = Symbol.Ask - SellStopDistance * Symbol.PipSize;
+
+            // Place Buy Stop order
+            PlaceStopOrder(TradeType.Buy, Symbol.Name, LotSize, buyStopPrice, "BuyStopNews", StopLossPips, TakeProfitPips);
+
+            // Place Sell Stop order
+            PlaceStopOrder(TradeType.Sell, Symbol.Name, LotSize, sellStopPrice, "SellStopNews", StopLossPips, TakeProfitPips);
+
+            Print("Pending orders placed at Buy Stop: {0}, Sell Stop: {1}", buyStopPrice, sellStopPrice);
+        }
+    }
+}
